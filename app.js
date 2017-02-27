3f3c706400bd15abf815856ae5083f9cd2eda70c
akkidg <akashg@zabuzalabs.com>
2017-02-27 17:26:21 +0530

    changed wrong variable senderId

--- app.js
+++ app.js
@@ -75,7 +75,6 @@ app.get('/webhook', function(req, res) {
   }  
 });
 
-
 /*
  * All callbacks for Messenger are POST-ed. They will be sent to the same
  * webhook. Be sure to subscribe your app to your page to receive callbacks
@@ -265,7 +264,7 @@ function receivedMessage(event) {
 
       case 'special':
         sendTypingOn(senderID);
-        sendAllSpecial(recipientId);
+        sendAllSpecial(senderID);
         break;
 
       case 'special dishes':
