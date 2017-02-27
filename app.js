54dd7e63e9e7307e37ff5dc03b37251a148a13e8
akkidg <akashg@zabuzalabs.com>
2017-02-27 17:03:40 +0530

    typo of changed

--- app.js
+++ app.js
@@ -944,7 +944,7 @@ function showReviews(recipientId){
           template_type: "generic",
           elements: [{
             title: "Christina R.",
-            subtitle:"This place gets busy! And it seems like there are some repeat customers because the waitress (the only waitress working) knew people's names. The waitress was FAST, efficient, patient, she was great for handling all of those tables. She was so chipper and happy too. The food was spot on, there is a reason why "famous" is in the name of their restaurant. You can sit inside or outside where there are tables out front. I've seen people bring their dogs with them to sit outside too. The entire staff is nice, even the nice guy that brings the food out. They do take out, and deliver ($50 min I believe).",
+            subtitle:"This place gets busy! And it seems like there are some repeat customers because the waitress (the only waitress working) knew people's names. The waitress was FAST, efficient, patient, she was great for handling all of those tables. She was so chipper and happy too. The food was spot on, there is a reason why 'famous' is in the name of their restaurant. You can sit inside or outside where there are tables out front. I've seen people bring their dogs with them to sit outside too. The entire staff is nice, even the nice guy that brings the food out. They do take out, and deliver ($50 min I believe).",
             item_url: "https://www.yelp.com/biz/famous-greek-salads-clearwater",  
             buttons: [{
               type: "postback",
